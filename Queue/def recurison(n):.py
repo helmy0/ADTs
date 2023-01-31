f8d2f1106d64ae36d9f195b71741e710977a2f70
Omar Farrag <omar.tarek.farrag123@gmail.com>
2023-01-31 16:43:03 +0300

    added stack OOP

--- Queue/def recurison(n):.py
+++
@@ -1,7 +0,0 @@
-def recurison(n):
-    if n == 1:
-        return 1
-    else:
-        return n * recurison(n-1)
-
-print(recurison(int(input"Enter a number")))
---
+++ Stack/stackNewOOP.py
@@ -0,0 +1,35 @@
+
+class stack():
+    
+    def __init__(self):
+        self.length = 0
+        self.arr = [None for i in range(10)]
+        self.currPointer = 0
+    def add(self, value):
+    
+        if self.arr[len(self.arr) - 1] != None:
+            print(f'The queue is full... \n {self.arr}')
+            return self.arr           
+        else:
+            self.arr[self.currPointer] = value
+            self.currPointer = self.currPointer + 1
+            print(self.arr)
+    def pop(self):
+        self.currPointer = self.currPointer - 1 
+        if self.arr[0] == None:
+            print('The queue is empty... \n {self.arr}')
+        else:
+            self.arr[self.currPointer] = None
+            print(self.arr) 
+q= stack()
+
+q.add(8)
+q.add(2)
+q.add(3)
+q.pop()
+q.pop()
+q.pop()
+q.pop()
+
+
+
